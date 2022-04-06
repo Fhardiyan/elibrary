619dbb54174585e92bce9ce8d87cc8190bc734f4
Fhardiyan <101076507+Fhardiyan@users.noreply.github.com>
2022-04-06 09:25:10 +0700

    update view

--- view.php
+++ view.php
@@ -37,27 +37,35 @@
 <main>
 
   <?php
-    $inc = 0;
     while ($data= $sql->fetch_assoc()) {
   ?>
 
   <!-- Modal Search -->
         <div id="id01" class="w3-modal">
-          <div class="w3-modal-content w3-card-4 w3-animate-zoom" style="max-width:600px">
-            <div class="w3-center"><br>
-              <span onclick="document.getElementById('id01').style.display='none'" class="w3-button w3-xlarge w3-hover-red w3-display-topright" title="Close Modal">&times;</span>
+          <div class="w3-modal-content w3-card-4 w3-animate-zoom" style="max-width:800px">
+            <div class="w3-container"><br>
+             <h2><center><b>Judul</b></center></h2>
+                <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="judul buku">
             </div>
-            <form class="w3-container">
-              <div class="w3-section">
+            <div class="w3-half w3-container">
                 <label><b>Kategori</b></label>
                 <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Kategori">
                 <label><b>Penerbit</b></label>
                 <input class="w3-input w3-border" type="text" placeholder="Penerbit">
-                <button class="w3-button w3-block w3-green w3-section w3-padding" type="submit">Search</button>
-              </div>
-            </form>
+                <label><b>ISBN</b></label>
+                <input class="w3-input w3-border" type="text" placeholder="ISBN">
+            </div>
+            <div class="w3-half w3-container">
+                <label><b>Pengarang</b></label>
+                <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Pengarang">
+                <label><b>Lokasi Buku</b></label>
+                <input class="w3-input w3-border" type="text" placeholder="Penerbit">
+                <label><b>Tanggal Input Buku</b></label>
+                <input class="w3-input w3-border" type="text" placeholder="Tanggal Input Buku">
+            </div>
 
             <div class="w3-container w3-border-top w3-padding-16 w3-light-grey">
+               <button class="w3-button w3-block w3-green w3-section w3-padding" type="submit">Search</button>
               <button onclick="document.getElementById('id01').style.display='none'" type="button" class="w3-button w3-red">Cancel</button>
             </div>
           </div>
@@ -67,13 +75,10 @@
           <div id="detail" class="w3-modal">
           <div class="w3-modal-content w3-card-4 w3-animate-zoom" style="max-width:900px">
             <div class="w3-center"><br>
-              <span onclick="document.getElementById('detail').style.display='none'" class="w3-button w3-xlarge w3-hover-red w3-display-topright" title="Close Modal">&times;</span>
             </div>
             <div class="w3-row">
               <div class="w3-quarter w3-container">
               <img src="upload/<?php echo $data['fotobuku'];?>" width="200" height="200">
-              <br><br><br><br><br>
-              <label><a href='buku/<?php echo $data['buku'] ?>'><img src='buku/logopdf.png' width="90" height="90"></a><?php echo $data['buku'] ?></label> 
               </div>
               <div class="w3-threequarter w3-container" style="text-align:left; color: black;">
                 <label><b>Judul</b></label>
@@ -100,36 +105,36 @@
 
         <!-- Modal Baca -->
           <div id="baca" class="w3-modal">
-          <div class="w3-modal-content w3-card w3-animate-zoom" style="max-width:900px">
+          <div class="w3-modal-content w3-card w3-animate-zoom" style="max-width:1400px">
             <div class="w3-center"><br>
               <span onclick="document.getElementById('baca').style.display='none'" class="w3-button w3-xlarge w3-hover-red w3-display-topright" title="Close Modal">&times;</span>
             </div>
+            <div class="w3-row">
+              <div class="w3-container">
+                  <object type="application/pdf" src="buku/CamScanner 01-07-2022 11.07.pdf" width="1400" height="450"></object>
+              </div>
+            </div> 
             <div class="w3-container w3-border-top w3-padding-16 w3-light-grey">
               <button onclick="document.getElementById('baca').style.display='none'" type="button" class="w3-button w3-red">Cancel</button>
             </div>
           </div>
         </div>
 
-  <?php if (($inc == 0) || ($inc % 4 == 0)){ ?>
-    <div class="row d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-  <?php } ?>
-    <div class="col-lg-3 flex-lg-equal bg-dark text-center text-white overflow-hidden">
+<!-- Main Page -->
+    <div class="flex-lg-row my-lg-3 ps-lg-3">
+    <div class="col-lg-4 flex-column bg-dark text-center text-white overflow-hidden">
         <h4 class="display-5"><?php echo $data['judul'];?></h4>
         <p class="lead"><?php echo $data['kategori'];?></p>
         <img src="upload/<?php echo  $data['fotobuku'];?>" width="200" height="200">
         <br><br><button onclick="document.getElementById('baca').style.display='block'" class="btn btn-primary">BACA</button>
         <button onclick="document.getElementById('detail').style.display='block'" class="btn btn-success">detail</button>
     </div>
-  <?php if (($inc == 0) || ($inc % 4 == 0)){ ?>
     </div>
-  <?php $inc = 0; } ?>
   <?php
-      $inc++;
     }
   ?>
 </main>
 
-    <script src="assets/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/dist/js/kumpulanjs.js"></script>       
+    <script src="assets/dist/js/bootstrap.bundle.min.js"></script>     
   </body>
 </html>
